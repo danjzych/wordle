76936e3023313897bd2964ac5c86e667354a664b
Daniel Zych <danjzych@gmail.com>
2023-10-03 10:58:58 -0500

    added some header styles

--- src/Header.js
+++ src/Header.js
@@ -5,11 +5,13 @@ import "./Header.css";
 function Header() {
   return (
     <header className="Header">
-      <div>
+      <div className="Header-Content Header-Logo">
         <h1>Wordle</h1>
       </div>
-      <div>
-        <p>Other nav stuff</p>
+      <div className="Header-Content">
+        <i class="bi bi-question-circle"></i>
+        <i class="bi bi-bar-chart-fill"></i>
+        <i class="bi bi-gear-fill"></i>
       </div>
     </header>
   );
