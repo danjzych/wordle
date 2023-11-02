47586205e8887cf27fd5b18d146b283030e412ce
Daniel Zych <danjzych@gmail.com>
2023-11-02 09:25:34 -0500

    Tweaked keyboard for later work, refactored cell now that context is no longer needed to prevent animations

--- src/components/Game/Cell.js
+++ src/components/Game/Cell.js
@@ -1,16 +1,8 @@
-import React, { useContext } from "react";
-import playingContext from "../../contexts/playingContext";
 import "./Cell.css";
 
 function Cell({ letter }) {
-  const { isPlaying } = useContext(playingContext);
-
   const classes = ["Cell"];
   classes.push(letter.status === undefined ? "empty" : letter.status);
-
-  if (isPlaying) {
-    classes.push("prevent-animation");
-  }
 
   return <td className={classes.join(" ")}>{letter.letter}</td>;
 }
--- src/components/Game/Keyboard.js
+++ src/components/Game/Keyboard.js
@@ -1,5 +1,19 @@
 import React from "react";
 
-function Keyboard() {}
+const keyboard = [
+  ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P", "DELETE"],
+  ["A", "S", "D", "F", "G", "H", "J", "K", "L", "ENTER"],
+  ["Z", "X", "C", "V", "B", "N", "M"],
+];
+
+function Keyboard() {
+  return (
+    <div>
+      {keyboard.flat().map((k) => (
+        <small>{k}</small>
+      ))}
+    </div>
+  );
+}
 
 export default Keyboard;
